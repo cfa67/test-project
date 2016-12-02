0f5ab1fe7acaf4981aa4dce644951d43e74bf8c1
fac <christian.facciorusso@kyberna.com>
2016-12-02 14:03:51 +0100

    First commit

---
+++ src/pages/podcast/podcast.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'podcast-page',
+  templateUrl: 'podcast.html'
+})
+export class PodcastPage {
+  constructor() {
+
+  }
+}
